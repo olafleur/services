e417c485721b1c8b965b1fb208e9e3e3cbcf165f
Olivier Lafleur <olivier.lafleur@elapsetech.com>
2018-10-26 11:01:26 -0400

    Service B

--- src/ServiceB.java
+++ src/ServiceB.java
@@ -3,34 +3,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class ServiceA {
-    private List<Emission> emissions;
-    private List<Serie> series;
+public class ServiceB {
+    private final List<Emission> emissions;
+    private final List<Serie> series;
 
-    public ServiceA() {
+    public ServiceB() {
+        series = new ArrayList<>();
         emissions = new ArrayList<>();
 
-        emissions.add(new Emission("1",
+        emissions.add(new Emission("A",
                 "Star Wars",
                 LocalDateTime.of(2018, 11, 4, 18, 0),
                 LocalDateTime.of(2018, 11, 4, 21, 0), null));
 
-        emissions.add(new Emission("2",
+        emissions.add(new Emission("B",
                 "Une émission",
                 LocalDateTime.of(2018, 11, 5, 18, 30),
                 LocalDateTime.of(2018, 11, 5, 19, 0), Optional.of(1)));
 
-        series = new ArrayList<>();
+        emissions.add(new Emission("C",
+                "Une émission",
+                LocalDateTime.of(2018, 11, 5, 18, 30),
+                LocalDateTime.of(2018, 11, 5, 19, 0), Optional.of(1)));
 
         series.add(new Serie(1, "The Big Bang Theory"));
-
-    }
-
-    public List recupererEmissions() {
-        return emissions;
-    }
-
-    public List recupererSeries() {
-        return series;
     }
 }
